7436f875dec0e14a0dc7968b54257f5a4a31af42
445654 <christophe.carmagnac@gmail.com>
2020-12-04 00:38:30 +0100

    Test

--- static/js/surfeur.js
+++ static/js/surfeur.js
@@ -1,5 +1,5 @@
 //const chambery = [45.64325, 5.8720];
-var postalMap = L.map("mapPollution").setView([45.64325, 5.8720], 10);
+/*var postalMap = L.map("mapPollution").setView([45.64325, 5.8720], 10);
 console.log("Yohann il dort");
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution: "&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a>"
@@ -12,3 +12,17 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
 })
     .addTo(postalMap)
     .bindPopup("Emplacement: " + item.postal + ", " + item.count + " personnes, " + dist + " km");*/
+var postalMap = L.map("mapPollution").setView([45.64325, 5.8720], 10);
+
+L.circle([45, 6], {
+    color: "#ff0000",
+    fillColor: "#ff0000",
+    fillOpacity: 0.5,
+    radius: 5
+})
+    .addTo(postalMap)
+    .bindPopup("Emplacement: ");
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution: "&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a>"
+}).addTo(postalMap);
