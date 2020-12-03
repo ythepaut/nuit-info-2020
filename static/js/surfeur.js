ce93cb5b4fba81d4e5ec9f973f45ae1ed807a336
445654 <christophe.carmagnac@gmail.com>
2020-12-04 00:28:07 +0100

    Fix

--- static/js/surfeur.js
+++ static/js/surfeur.js
@@ -1,6 +1,9 @@
 //const chambery = [45.64325, 5.8720];
 var postalMap = L.map("mapPollution").setView([45.64325, 5.8720], 10);
 
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution: "&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a>"
+}).addTo(postalMap);
 /*L.circle([lon, lat], {
     color: ,
     fillColor: ,
