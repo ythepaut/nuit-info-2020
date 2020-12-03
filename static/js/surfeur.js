752a8d1c161942d6b9590831538ba02fd4aff62e
445654 <christophe.carmagnac@gmail.com>
2020-12-04 00:52:01 +0100

    Compatibilité Luchat

--- static/js/surfeur.js
+++ static/js/surfeur.js
@@ -14,14 +14,18 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     .bindPopup("Emplacement: " + item.postal + ", " + item.count + " personnes, " + dist + " km");*/
 var postalMap = L.map("mapPollution").setView([45.64325, 5.8720], 5);
 
-L.circle([45, 6], {
-    color: "#ff0000",
-    fillColor: "#ff0000",
-    fillOpacity: 0.5,
-    radius: 5
-})
-    .addTo(postalMap)
-    .bindPopup("Emplacement: ");
+var data = [];
+
+for (elt in data) {
+    L.circle([45, 6], {
+        color: "#ff0000",
+        fillColor: "#ff0000",
+        fillOpacity: 0.5,
+        radius: 5
+    })
+        .addTo(postalMap)
+        .bindPopup("Emplacement: ");
+}
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution: "&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a>"
