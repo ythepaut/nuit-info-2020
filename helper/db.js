43ed2a05da45ffc197477ceb3a919f54472fb3a3
Yohann THEPAUT <contact@ythepaut.com>
2020-12-03 16:23:04 +0100

    Oubli 25b65a6b6f27bb397c10de56c81cfada0f671917

--- helper/db.js
+++ helper/db.js
@@ -3,7 +3,7 @@ module.exports = (callback) => {
     const MongoClient = require("mongodb").MongoClient;
 
     MongoClient.connect(
-        "mongodb://readonly:E0i2Dy5w9YQwOBrozvqe@localhost:27017/db",//TODO replace db name
+        `mongodb://readonly:${process.env.DB_PASSWORD}@localhost:27017/db`,//TODO replace db name
         {useUnifiedTopology : true},
         (err, client) => {
             if (err) {
