8e0e8d5f38c2d307838dfaf7b5e2df18e4626643
445654 <christophe.carmagnac@gmail.com>
2020-12-04 02:13:56 +0100

    Ajout map rapport

--- static/js/surfeur.js
+++ static/js/surfeur.js
@@ -13,6 +13,7 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     .addTo(postalMap)
     .bindPopup("Emplacement: " + item.postal + ", " + item.count + " personnes, " + dist + " km");*/
 var postalMap = L.map("mapPollution").setView([45.583843, 5.907417], 5);
+var rapportMap = L.map("mapRapport").setView([45.583843, 5.907417], 5);
 
 L.circle([45.583843, 5.907417], {
     color: "#ff0000",
@@ -54,7 +55,12 @@ var markMap = function(url, map) {
 }
 
 markMap("http://nuitinfo2020.ythepaut.com:8001/api/pollution", postalMap);
+markMap("http://nuitinfo2020.ythepaut.com:8001/api/rapport", rapportMap);
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution: "&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a>"
 }).addTo(postalMap);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution: "&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a>"
+}).addTo(rapportMap);
