2e44e8ea7dad55f0251740a34c1afb799274d57c
445654 <christophe.carmagnac@gmail.com>
2020-12-04 01:37:28 +0100

    Lien API pollution

--- static/js/surfeur.js
+++ static/js/surfeur.js
@@ -21,20 +21,39 @@ L.circle([45.583843, 5.907417], {
     radius: 5
 })
     .addTo(postalMap)
-    .bindPopup("Emplacement: ");
+    .bindPopup("Fromage");
 
-var data = [];
+var markMap = function(url, map) {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var data = JSON.parse(this.responseText);
+            var max = data[0].pollution;
 
-for (elt in data) {
-    L.circle([45, 6], {
-        color: "#ff0000",
-        fillColor: "#ff0000",
-        fillOpacity: 0.5,
-        radius: 5
-    })
-        .addTo(postalMap)
-        .bindPopup("Emplacement: ");
+            for (var i = 1; i < data.length; i++) {
+                if (data[i].value > max) {
+                    max = data[i].value;
+                }
+            }
+
+            for (var i = 0; i < data.length; i++) {
+                L.circle(data[i]._id, {
+                    color: "#ff0000",
+                    fillColor: "#ff0000",
+                    fillOpacity: 0.5,
+                    radius: (data[i].value/max)*25
+                })
+                    .addTo(map);
+                    //.bindPopup("Emplacement: ");
+            }
+
+        }
+    };
+    xmlhttp.open("GET", url);
+    xmlhttp.send();
 }
+
+markMap("http://nuitinfo2020.ythepaut.com:8001/api/pollution", postalMap);
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution: "&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a>"
